7bf3ccb38382bd7bca6f4c7db89ebe079d983f9b
Michael Johnson Owallah <51329456+Owallah@users.noreply.github.com>
2023-05-02 01:45:34 +0300

    Revert "Done"

--- phase-2-group-project-openwide/src/components/Search.jsx
+++ phase-2-group-project-openwide/src/components/Search.jsx
@@ -1,37 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react'
 
-const Search = ({ playlist, setFilteredPlaylist }) => {
-  const [searchTerm, setSearchTerm] = useState('');
-
-  const handleInputChange = (event) => {
-    setSearchTerm(event.target.value);
-  };
-
-  const handleCategoryChange = (event) => {
-    setSearchCategory(event.target.value);
-  };
-
-  const handleSearchClick = () => {
-    const filteredPlaylist = playlist.filter(
-      (playlist) =>
-        playlist.category.toLowerCase().includes(searchCategory.toLowerCase())
-    );
-    setFilteredPlaylist(filteredPlaylist);
-  };
-
+const Search = () => {
   return (
-    <div>
-      <input type="text" value={searchTerm} onChange={handleInputChange} placeholder="Tafuta nyimbo" />
-      <select value={searchCategory} onChange={handleCategoryChange}>
-        <option value="">Filter</option>
-        <option value="id">id</option>
-        <option value="name">Name</option>
-        <option value="uNm">uNm</option>
-        <option value="Text">Text</option>
-      </select>
-      <button onClick={handleSearchClick}>Search</button>
-    </div>
-  );
-};
+    <div>Search</div>
+  )
+}
 
-export default Search;
+export default Search
