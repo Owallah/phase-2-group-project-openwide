31e8538d262ea2fcfa153f3ab02ed4e0b7f2786f
Owallah <mikesonowallah@gmail.com>
2023-05-01 22:48:52 +0300

    Added functionality for Navigation and routing

--- phase-2-group-project-openwide/src/components/Navigation.jsx
+++ phase-2-group-project-openwide/src/components/Navigation.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navigation = () => {
   return (
-    <div>Navigation</div>
+    <nav>
+        <Link to={'/'} >Home</Link>
+        <Link to={'/playlist'} >Playlist</Link>
+        <Link to={'/about'} >About</Link>
+    </nav>
   )
 }
 
