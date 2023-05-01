8aa59580b324d620a3b2ecd1ddaef7e4f8ee0ae8
brianrono <brianrono16@gmail.com>
2023-05-01 23:43:32 +0300

    Workflow

--- phase-2-group-project-openwide/src/components/Playlist.jsx
+++ phase-2-group-project-openwide/src/components/Playlist.jsx
@@ -1,16 +1,20 @@
-import React from 'react'
-import Search from './Search'
-import SongItem from './SongItem'
+import React, { useState } from 'react';
+import Search from './Search';
+import SongItem from './SongItem';
 
-const Playlist = ({playList, setPlayList}) => {
+const Playlist = ({ playList, setPlayList }) => {
+  const [filteredPlaylist, setFilteredPlaylist] = useState(playList);
+  
   return (
     <>
-    <Search playList={playList} setPlayList={setPlayList} />
-    <div className="playlists">
-        <SongItem />
-    </div>
+      <Search playlist={playList} setFilteredPlaylist={setFilteredPlaylist} />
+      <div className="playlists">
+        {filteredPlaylist.map((song) => (
+          <SongItem key={song.id} song={song} />
+        ))}
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Playlist
+export default Playlist;
