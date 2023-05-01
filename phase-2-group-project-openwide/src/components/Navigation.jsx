ec4b31cf356c532ca0e1ea3fa6805e18327fb38d
Owallah <mikesonowallah@gmail.com>
2023-05-02 00:32:19 +0300

    Reworked the navigation, added an icon and title for the site.

--- phase-2-group-project-openwide/src/components/Navigation.jsx
+++ phase-2-group-project-openwide/src/components/Navigation.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
+import logo from '../assets/carjam.svg'
 
 const Navigation = () => {
   return (
     <nav>
-        <Link to={'/'} >Home</Link>
-        <Link to={'/playlist'} >Playlist</Link>
-        <Link to={'/about'} >About</Link>
+        <NavLink ><img src={logo} alt="open wide" to={'/'} /></NavLink>
+        <NavLink className='site-title' to={'/'} >Open Wide</NavLink>
+        <ul>
+            <li><NavLink to={'/'} >Home</NavLink></li>
+            <li><NavLink to={'/playlist'} >Playlist</NavLink></li>
+            <li><NavLink to={'/about'} >About</NavLink></li>
+        </ul>
+        
+        
+        
     </nav>
   )
 }
