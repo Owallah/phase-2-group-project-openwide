48e3d638f618dccf04bdb6480b2806ef21763b58
Nkatha_Glory <kinyua6945@gmail.com>
2023-05-02 10:08:52 +0300

    Added functionality by searching through the playlist

--- phase-2-group-project-openwide/src/components/Search.jsx
+++ phase-2-group-project-openwide/src/components/Search.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-const Search = () => {
+const Search = ({ playlist, setFilteredPlaylist }) => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleInputChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleCategoryChange = (event) => {
+    setSearchCategory(event.target.value);
+  };
+
+  const handleSearchClick = () => {
+    const filteredPlaylist = playlist.filter(
+      (playlist) =>
+        playlist.category.toLowerCase().includes(searchCategory.toLowerCase())
+    );
+    setFilteredPlaylist(filteredPlaylist);
+  };
+
   return (
-    <div>Search</div>
-  )
-}
+    <div>
+      <input type="text" value={searchTerm} onChange={handleInputChange} placeholder="Tafuta nyimbo" />
+      <select value={searchCategory} onChange={handleCategoryChange}>
+        <option value="">Filter</option>
+        <option value="id">id</option>
+        <option value="name">Name</option>
+        <option value="uNm">uNm</option>
+        <option value="Text">Text</option>
+      </select>
+      <button onClick={handleSearchClick}>Search</button>
+    </div>
+  );
+};
 
-export default Search
+export default Search;
