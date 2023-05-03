1b58d14e27cc08f3e849333f84f61cfd66381654
Owallah <mikesonowallah@gmail.com>
2023-05-04 02:58:41 +0300

    Revamped the song item, added some icon buttons and functionalities.

--- phase-2-group-project-openwide/src/components/About.jsx
+++ phase-2-group-project-openwide/src/components/About.jsx
@@ -13,14 +13,18 @@ const About = () => {
             <h2 className="section-title">
               The Awesome Team Behind Open Wide
             </h2>
-            <p className="section-subtitle">Lorem ipsum dolor sit amet consectetur adipisicing elit. <br /> Quibusdam ratione similique ipsa excepturi soluta iure praesentium quis. Molestias dicta sit debitis, distinctio expedita minus eaque porro nam quod quasi illo.</p>
+            <p className="section-subtitle">
+              The ideation and realization of this project was brought to succes by: <br /> The members of <span id='group-name'>group 3</span>. <br />
+              These members worked tirelessly to make sure you have a fully functional app that suites your music needs. <br />
+              They are presented bellow:
+              </p>
           </div>
           <div className="col-sm-6 col-md-4">
             <div className="team-item">
               <img className='team-img' src={glory} alt="Glory Nkatha" />
               <h3>Glory Nkatha</h3>
               <div className="team-info">
-                <p>Senior React Dev</p>
+                <p>Senior React Dev <br />Senior UI/UX Designer</p>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed nobis, voluptate vel maiores voluptates culpa? Obcaecati provident similique consequuntur. </p>
 
                 <ul className="team-icons">
@@ -56,7 +60,7 @@ const About = () => {
               <img className='team-img' src={brian} alt="Brian Rono" />
               <h3>Brian Rono</h3>
               <div className="team-info">
-                <p>Senior React Dev</p>
+                <p>Full Stack <br /> Software Eng</p>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed nobis, voluptate vel maiores voluptates culpa? Obcaecati provident similique consequuntur. </p>
 
                 <ul className="team-icons">
@@ -92,7 +96,7 @@ const About = () => {
               <img className='team-img' src={salad} alt="Mohamed Salanka" />
               <h3>Mohamed Salanka</h3>
               <div className="team-info">
-                <p>Senior React Dev</p>
+                <p>Senior React Dev <br />Full Stack Software Engineer</p>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed nobis, voluptate vel maiores voluptates culpa? Obcaecati provident similique consequuntur. </p>
 
                 <ul className="team-icons">
@@ -128,7 +132,7 @@ const About = () => {
               <img className='team-img' src={johnson} alt="Michael Johnson" />
               <h3>Michael Johnson</h3>
               <div className="team-info">
-                <p>Senior React Dev</p>
+                <p>Senior React Dev <br />Senior DevOps Engineer</p>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed nobis, voluptate vel maiores voluptates culpa? Obcaecati provident similique consequuntur. </p>
 
                 <ul className="team-icons">
--- phase-2-group-project-openwide/src/components/SongItem.jsx
+++ phase-2-group-project-openwide/src/components/SongItem.jsx
@@ -1,19 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function SongItem({ song }) {
-  return (
-    <div>
+  const [isLiked, setIsLiked] = useState(false);
+  console.log(song);
+  const str = song.eId;
+  const id = str.split("/").pop();
+  
+  const nameString = song.name;
+  const firstPart = nameString.split("-")[0].trim();
+
+  //handle like 
+    const handleClick = () => {
+      setIsLiked(!isLiked);
+    };
+
+    const iconClass = `fa fa-heart${isLiked ? '' : '-o'}`;
+
+    //handle share
+    const handleShare = (e) => {
+      e.preventDefault();
+  
+      if (navigator.share) {
+        navigator.share({
+        title: `Checkout this ${song.name} song by ${firstPart}`,
+        url: `https://www.youtube.com/watch?v=${id}`,
+      });
+      } else {
+        navigator.clipboard.writeText(`https://www.youtube.com/watch?v=${id}`)
+        .then(() => {
+          alert('URL copied to clipboard!');
+        })
+        .catch((err) => {
+          alert('Error copying URL to clipboard: ' + err);
+        });
+    }
+      
+    };
     
-          <h2>{song.name}</h2>
-          <p>Artist: {song.artist}</p>
-          <p>Album: {song.album}</p>
-          <p>Genre: {song.genre}</p>
-          <audio controls>
-            <source src={song.url} type="audio/mp3" />
-          </audio>
-          {song.thumbnail && (
-        <img src={song.thumbnail.url} alt={song.name} />
+  return (
+    <div className='song-item'>
+          {song.img && (
+        <img src={song.img} alt={song.name} />
           )}
+          <div className="song-details">
+            <h4 className="artist">{firstPart}</h4>
+          <h5 className='song-name'>{song.name}</h5>
+          <div className="song-button-section">
+            <a href="#"><i class="fa fa-plus" aria-hidden="true"></i></a>
+            <a href="#" onClick={handleClick}><i class={iconClass} aria-hidden="true"></i></a>
+            <a href={`https://www.youtube.com/watch?v=${id}`} target='_blank'><i class="fa fa-youtube-play" aria-hidden="true"></i></a>
+            <a href="#" onClick={handleShare}><i class="fa fa-share-alt" aria-hidden="true"></i></a>
+          </div>
+          </div>
 
     </div>
   )
