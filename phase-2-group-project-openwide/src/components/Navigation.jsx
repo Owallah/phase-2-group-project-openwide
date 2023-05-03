e84075c2d08abaf2450465271447bc11e379d218
Owallah <mikesonowallah@gmail.com>
2023-05-03 03:47:10 +0300

    Did some code refactor and update

--- phase-2-group-project-openwide/src/components/Navigation.jsx
+++ phase-2-group-project-openwide/src/components/Navigation.jsx
@@ -12,10 +12,10 @@ const Navigation = () => {
             <li><NavLink to={'/playlist'} >Playlist</NavLink></li>
             <li><NavLink to={'/about'} >About</NavLink></li>
         </ul>
-            <div class="container">
+            <div className="container">
               <label id='toggle-dark-theme'>Toggle Dark Theme</label>
               <input id="checkbox" name="checkbox" type="checkbox" />
-            < label class="label" for="checkbox"></label>
+            < label className="label" htmlFor="checkbox"></label>
             </div>
 
 
