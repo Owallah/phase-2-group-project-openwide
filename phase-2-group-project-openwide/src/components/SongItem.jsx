2cc6116fc0b15ace227aa1b3eb88984f956b1b80
Mohamed <mohasalanka2020@gmail.com>
2023-05-03 07:54:18 +0300

    Done with songItem

--- phase-2-group-project-openwide/src/components/SongItem.jsx
+++ phase-2-group-project-openwide/src/components/SongItem.jsx
@@ -1,24 +1,17 @@
 import React from 'react'
 
-function Playlist({ playList }) {
+function SongItem({ song }) {
   return (
     <div>
-      <h1>Playlist</h1>
-      <ul>
-        {playList.map((song, index) => (
-          <li key={index}>
-            <h2>{song.name}</h2>
-            <p>Artist: {song.artist}</p>
-            <p>Album: {song.album}</p>
-            <p>Genre: {song.genre}</p>
-            <audio controls>
-              <source src={song.url} type="audio/mp3" />
-            </audio>
-          </li>
-        ))}
-      </ul>
-    </div>
+          <h2>{song.name}</h2>
+          <p>Artist: {song.artist}</p>
+          <p>Album: {song.album}</p>
+          <p>Genre: {song.genre}</p>
+          <audio controls>
+            <source src={song.url} type="audio/mp3" />
+          </audio>
+      </div>
   )
 }
 
-export default Playlist
+export default SongItem
