d4ff04e3666647455e8a8acf00dc651fe6ec5691
Nkatha_Glory <kinyua6945@gmail.com>
2023-05-04 08:27:24 +0300

    Added a search button

--- phase-2-group-project-openwide/src/components/Searches.jsx
+++ phase-2-group-project-openwide/src/components/Searches.jsx
@@ -1,40 +1,22 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 
-const Search = ({ playlist, setFilteredPlaylist }) => {
-  const [searchTerm, setSearchTerm] = useState('')
-  const [searchCategory, setSearchCategory] = useState('')
+const SearchButton = ({ handleSearch }) => {
+  const [searchTerm, setSearchTerm] = useState('');
 
   const handleInputChange = (event) => {
     setSearchTerm(event.target.value);
   };
 
-  const handleCategoryChange = (event) => {
-    setSearchCategory(event.target.value);
+  const handleSearchClick = () => {
+    handleSearch(searchTerm);
   };
 
-  const handleSearchClick = () => {
-    const filteredPlaylist = playlist.filter((item) => {
-      if (searchCategory === '') {
-        return item.name.toLowerCase().includes(searchTerm.toLowerCase());
-      } else {
-        return item[searchCategory].toLowerCase().includes(searchTerm.toLowerCase());
-      }
-    });
-    setFilteredPlaylist(filteredPlaylist);
-  };
-  
   return (
     <div>
-      <input type="text" value={searchTerm} onChange={handleInputChange} placeholder="Tafuta nyimbo" />
-      <select value={searchCategory} onChange={handleCategoryChange}>
-        <option value="id">id</option>
-        <option value="name">Name</option>
-        <option value="uNm">Artist</option>
-        <option value="Text">Text</option>
-      </select>
+      <input type="text" value={searchTerm} onChange={handleInputChange} />
       <button onClick={handleSearchClick}>Search</button>
     </div>
   );
 };
 
-export default Search;
+export default SearchButton;
